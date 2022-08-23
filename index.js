7f040d18f5b3135cb6bc0701ff43f0f203a0127f
Alana Caporale <38472545+alanacapcreates@users.noreply.github.com>
2022-08-22 23:25:01 -0400

    fixed mobile validation

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const validMsg = document.getElementById("valid-msg");
 const errorIcon = document.getElementById("error-icon");
 const validIcon = document.getElementById("valid-icon");
 
+const errorMsgMobile = document.getElementById("error-msg-mobile");
+const validMsgMobile = document.getElementById("valid-msg-mobile");
+const errorIconMobile = document.getElementById("error-icon-mobile");
+const validIconMobile = document.getElementById("valid-icon-mobile");
+
   
 errorIcon.style.display='none';
 errorMsg.style.display = 'none';
@@ -39,17 +44,17 @@ function validateDesktop(e){
 function validateMobile(e){
 
   if(!userInputMobile.value){
-    validIcon.style.display= 'none';
-    validMsg.style.display = 'none';
-    errorMsg.style.display = 'block';
-    errorIcon.style.display = 'block';
+    validIconMobile.style.display= 'none';
+    validMsgMobile.style.display = 'none';
+    errorMsgMobile.style.display = 'block';
+    errorIconMobile.style.display = 'block';
 
   }
   else if(userInputMobile.value){
-    errorMsg.style.display = 'none';
-    errorIcon.style.display = 'none';
-    validMsg.style.display = 'block';
-    validIcon.style.display= 'block';
+    errorMsgMobile.style.display = 'none';
+    errorIconMobile.style.display = 'none';
+    validMsgMobile.style.display = 'block';
+    validIconMobile.style.display= 'block';
   }
   
 }

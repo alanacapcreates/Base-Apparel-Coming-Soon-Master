605ee4d019c5cf2da5f63f272f610ebd070a8369
Alana Caporale <38472545+alanacapcreates@users.noreply.github.com>
2022-08-29 14:55:43 -0400

    Added BG image

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const validMsgMobile = document.getElementById("valid-msg-mobile");
 const errorIconMobile = document.getElementById("error-icon-mobile");
 const validIconMobile = document.getElementById("valid-icon-mobile");
 
-  
+
 errorIcon.style.display='none';
 errorMsg.style.display = 'none';
 validMsg.style.display = 'none';
@@ -20,38 +20,29 @@ let userInputMobile = document.getElementById("searchBarInputMobile")
 
 
 function validateDesktop(e){
-  
+  // setting default
   errorIcon.style.display='none';
   errorMsg.style.display = 'none';
   validMsg.style.display = 'none';
   validIcon.style.display= 'none';   
 
-
-  // if(!userInputDesktop.value){
-  //   validIcon.style.display= 'none';
-  //   validMsg.style.display = 'none';
-  //   errorMsg.style.display = 'block';
-  //   errorIcon.style.display = 'block';
-
-  // }
-  // else if(userInputDesktop.value){
-  //   if(userInputDesktop.value.includes("@")){
-  //     errorMsg.style.display = 'none';
-  //     errorIcon.style.display = 'none';
-  //     validMsg.style.display = 'block';
-  //     validIcon.style.display= 'block';
-
-  //   }
-  // }
+  if(userInputDesktop.value == ""){
+    validIcon.style.display= 'none';
+    validMsg.style.display = 'none';
+    errorMsg.style.display = 'block';
+    errorIcon.style.display = 'block';
+  }
 
   if(userInputDesktop.value){
-    if(!userInputDesktop.value.includes("@")){
+    // if there is not '@'
+    if(!userInputDesktop.value.includes("@")) {
     validIcon.style.display= 'none';
     validMsg.style.display = 'none';
     errorMsg.style.display = 'block';
     errorIcon.style.display = 'block';
     }
   }
+  // if there *is* an '@'
   if(userInputDesktop.value.includes("@")){
     errorMsg.style.display = 'none';
     errorIcon.style.display = 'none';
@@ -66,6 +57,12 @@ function validateMobile(e){
   validMsgMobile.style.display = 'none';
   validIconMobile.style.display= 'none';
 
+  if(userInputDesktop.value == ""){
+    validIconMobile.style.display= 'none';
+    validMsgMobile.style.display = 'none';
+    errorMsgMobile.style.display = 'block';
+    errorIconMobile.style.display = 'block';
+  }
   if(userInputMobile.value){
     if(!userInputMobile.value.includes("@")){
     validIconMobile.style.display= 'none';
@@ -80,12 +77,4 @@ function validateMobile(e){
     validMsgMobile.style.display = 'block';
     validIconMobile.style.display= 'block';
     }
-  // else if(userInputMobile.value){
-  //   if(userInputMobile.value.includes("@")){
-  //   errorMsgMobile.style.display = 'none';
-  //   errorIconMobile.style.display = 'none';
-  //   validMsgMobile.style.display = 'block';
-  //   validIconMobile.style.display= 'block';
-  //   }
-  // }
 }
